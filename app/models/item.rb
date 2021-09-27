fc8b0227e3acb6b359423c5b183b09509063eb90
Taka120 <dark.colors1542@gmail.com>
2021-09-27 18:02:40 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name, :string, presence: true, message: "can't be blank"
+  validates :explanation, :text, presence: true, message: "can't be blank"
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, :integer, presence:true, message: "can't be blank"
+    format:{with: /\A[0-9]+\z/, message: "is invalid"}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 , message: "is out of setting range" }
 end
