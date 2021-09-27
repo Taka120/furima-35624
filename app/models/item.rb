c777c199bbe3a6c694dd9e01cee423a10ae7e1ed
Taka120 <dark.colors1542@gmail.com>
2021-09-27 16:49:29 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :ays_to_ship
+
   belongs_to :user
   has_one_attached :image
 end
