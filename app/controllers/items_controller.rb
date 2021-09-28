84bd7420d9813230328cedf7040dc589ed695eca
Taka120 <dark.colors1542@gmail.com>
2021-09-28 21:17:53 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,23 @@
 class ItemsController < ApplicationController
+  def index
+  end
+
   def new
-    
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :delivery_fee_id, :area_id, :days_to_ship_id, :item_price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,16 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :delivery_fee
   belongs_to :area
-  belongs_to :ays_to_ship
-
+  belongs_to :DaysToShip
   belongs_to :user
   has_one_attached :image
 
-  validates :item_name, :string, presence: true, message: "can't be blank"
-  validates :explanation, :text, presence: true, message: "can't be blank"
+  validates :item_name, presence: true
+  validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, :integer, presence:true, message: "can't be blank", format:{with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :item_price, format:{with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
