19d562a8f570c5e7ee68572e26b2cc1add21e80e
Taka120 <dark.colors1542@gmail.com>
2021-10-01 17:20:30 +0900

    指摘事項の修正(バリデーションのリファクタリング)

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,20 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
   belongs_to :user
   has_one_attached :image
+  
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :area_id
+    validates :days_to_ship_id
+  end
 
-  validates :item_name, presence: true
-  validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true do
+    validates :item_name
+    validates :explanation
+    validates :image
+  end
+  
   validates :item_price, format:{with: /\A[0-9]+\z/}, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :image, presence: true
 end
