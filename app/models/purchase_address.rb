dcf87864aac3d5a881667b01413f38f6e9758908
Taka120 <dark.colors1542@gmail.com>
2021-10-05 21:06:54 +0900

    購入機能(purchase_address)のテストコードの実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,10 +6,10 @@ class PurchaseAddress
     validates :user_id
     validates :item_id
     validates :postal_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :municipalities
     validates :address
-    validates :telephone_number, format: {with: /\A\d{10,11}\z/}
+    validates :telephone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid"}
   end
   validate :building_name
 
---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchase_address do
+    user_id {1}
+    item_id {1}
+    postal_number { '123-4567' }
+    prefecture_id { 3 }
+    municipalities { 'テスト市' }
+    address { 'テスト群１ー１ー１' }
+    building_name { 'テストビル' }
+    telephone_number { '1234567890' }
+  end
+end
